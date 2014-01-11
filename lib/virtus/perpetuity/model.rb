cd5e8a89188bcad8613b87b8db010bd8bf33c09c
Craig Buchek <craig@boochtek.com>
2014-01-10 19:10:05 -0600

    Add Perpetuity mapping when Virtus::Perpetuity.model is included.

--- lib/virtus/perpetuity/model.rb
+++ lib/virtus/perpetuity/model.rb
@@ -1,8 +1,13 @@
+require 'perpetuity'
+
 module Virtus
   module Perpetuity
     module Model
       def self.included(base)
         base.send(:include, Virtus.model)
+        ::Perpetuity.generate_mapper_for base do
+          # TODO: Declare attributes.
+        end
       end
     end
   end
--- spec/basic_spec.rb
+++ spec/basic_spec.rb
@@ -1,4 +1,5 @@
 require 'virtus/perpetuity'
+require 'perpetuity'
 
 describe 'Including Virtus::Perpetuity.model' do
   before do
@@ -14,5 +15,9 @@ describe 'Including Virtus::Perpetuity.model' do
 
   it 'works' do
     expect { Examples::User.new(name:'Craig') }.not_to raise_exception
+  end
+
+  it 'creates a mapping' do
+    expect(Perpetuity[Examples::User]).not_to be_nil
   end
 end
